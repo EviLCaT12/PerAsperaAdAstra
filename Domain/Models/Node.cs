fcb4ea38a9b8d244770d23b4c3a173eaf49560aa
Timofey Kuznetsov <kuznetsov.timofey.2003@mail.ru>
2024-07-22 22:21:37 +1000

    create domain node model

---
+++ Domain/Models/Node.cs
@@ -0,0 +1,8 @@
+namespace Domain.Models;
+
+public class Node
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedDate { get; set; }
